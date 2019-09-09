414f6c54080ac5119271f49633bdbd8d1a31c427
LeeSeungCheon <koogk7@gmail.com>
2019-09-09 20:59:56 +0900

    분산원장 쓰기 모듈화

--- hyperledger_fabric/fabric_helper.js
+++ hyperledger_fabric/fabric_helper.js
@@ -3,8 +3,11 @@
 // call the packages we need
 const Fabric_Client = require('fabric-client');
 const CONSTANT = require('../common/constant');
+const util = require('util');
+
 
 // init Object : 분산원장에 접근 할 인증된 유저 객체를 초기화
+// mutable function
 module.exports.initObject = (user_client) => {
     let fabric_client = user_client.client;
     const store_path = CONSTANT.FABRIC_WALLET_PATH;
@@ -45,13 +48,12 @@ module.exports.initObject = (user_client) => {
             targets.push(peer);
             eventHubs.push(channel.newChannelEventHub(peer));
 
-            return {
-                "channel": channel,
-                "targets": targets,
-                "client": fabric_client,
-                "event_hubs": eventHubs,
-                "tx_id": fabric_client.newTransactionID()
-            };
+            user_client.channel = channel;
+            user_client.tx_id = fabric_client.newTransactionID();
+            user_client.event_hubs = eventHubs;
+            user_client.targets = targets;
+
+            return user_client;
         })
 };
 
@@ -74,17 +76,96 @@ module.exports.queryByChainCode= (ClientObj, chainCodeName, args) => {
     return channel.queryByChaincode(request);
 };
 
-module.exports.invokeByChainCode = (ClientObj, chainCodeName, args) => {
-    let tx_id = ClientObj['client'].newTransactionID();
-    let channel = ClientObj['channel'];
+// invoke
+module.exports.invokeByChainCode = (user_client, chainCodeName, args) => {
+    let order = user_client.client.newOrderer(CONSTANT.FABRIC_ODERER_URL);
+    user_client.channel.addOrderer(order);
+    console.log("Assigning transaction_id: ", user_client.tx_id.getTransactionID());
 
     const request = {
-        chaincodeId: 'tuna-app',
-        txId: tx_id,
+        //targets : --- letting this default to the peers assigned to the channel
+        chaincodeId: CONSTANT.FABRIC_CHAIN_CODE_ID,
         fcn: chainCodeName,
         args: args,
-        targets: ClientObj['targets']
+        chainId: CONSTANT.FABRIC_CHANEL_NAME,
+        txId: user_client.tx_id
     };
+    // send the transaction proposal to the peers
+    return user_client.channel.sendTransactionProposal(request)
+        .then((results) => {
+            let proposalResponses = results[0];
+            let proposal = results[1];
+            let header = results[2];
 
-    return channel.sendTransactionProposal(request);
+            let isProposalGood = false;
+            if (proposalResponses && proposalResponses[0].response &&
+                proposalResponses[0].response.status === 200) {
+                isProposalGood = true;
+                console.log('Transaction proposal was good');
+            } else {
+                console.error('Transaction proposal was bad');
+            }
+            if (isProposalGood) {
+                console.log(util.format(
+                    'Successfully sent Proposal and received ProposalResponse: Status - %s, message - "%s"',
+                    proposalResponses[0].response.status, proposalResponses[0].response.message));
+
+                // build up the request for the orderer to have the transaction committed
+                let request = {
+                    proposalResponses: proposalResponses,
+                    proposal: proposal,
+                    header: header
+                };
+
+                // set the transaction listener and set a timeout of 30 sec
+                // if the transaction did not get committed within the timeout period,
+                // report a TIMEOUT status
+                let transaction_id_string = user_client.tx_id.getTransactionID(); //Get the transaction ID string to be used by the event processing
+                let promises = [];
+                // 클라이언트측의 검증이 올바르다면 Proposal 결과 커밋 요청
+                let sendPromise = user_client.channel.sendTransaction(request);
+                promises.push(sendPromise); //we want the send transaction first, so that we know where to check status
+
+                // get an eventhub once the fabric client has a user assigned. The user
+                // is required bacause the event registration must be signed
+
+                // using resolve the promise so that result status may be processed
+                // under the then clause rather than having the catch clause process
+                // the status
+                let txPromise = new Promise((resolve, reject) => {
+                    let event_hub = user_client.event_hubs[0];
+                    let handle = setTimeout(() => {
+                        event_hub.disconnect();
+                        resolve({event_status : 'TIMEOUT'}); //we could use reject(new Error('Trnasaction did not complete within 30 seconds'));
+                    }, 3000);
+                    event_hub.connect();
+                    event_hub.registerTxEvent(transaction_id_string, (tx, code) => {
+                        // this is the callback for transaction event status
+                        // first some clean up of event listener
+                        clearTimeout(handle);
+                        event_hub.unregisterTxEvent(transaction_id_string);
+                        event_hub.disconnect();
+
+                        // 하이퍼레저 패브릭측에서 검증 결과 확인
+                        var return_status = {event_status : code, tx_id : transaction_id_string};
+                        if (code !== 'VALID') {
+                            console.error('The transaction was invalid, code = ' + code);
+                            resolve(return_status); // we could use reject(new Error('Problem with the tranaction, event status ::'+code));
+                        } else {
+                            console.log('The transaction has been committed on peer ' + event_hub.getPeerAddr());
+                            resolve(return_status);
+                        }
+                    }, (err) => {
+                        //this is the callback if something goes wrong with the event registration or processing
+                        reject(new Error('There was a problem with the eventhub ::'+err));
+                    });
+                });
+                promises.push(txPromise);
+
+                return Promise.all(promises);
+            } else {
+                console.error('Failed to send Proposal or receive valid response. Response null or status is not 200. exiting...');
+                throw new Error('Failed to send Proposal or receive valid response. Response null or status is not 200. exiting...');
+            }
+        })
 };
---
+++ hyperledger_fabric/test/invoke.js
@@ -0,0 +1,58 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+// call the packages we need
+const Fabric_Client = require('fabric-client');
+const CONSTANT = require('../common/constant');
+const FabricHelper = require('./fabric_helper');
+const util = require('util');
+
+async function main() {
+    let user_client = {
+        "client" : new Fabric_Client(),
+        "channel" : null,
+        "order" : null,
+        "event_hubs" : null,
+        "tx_id": null
+    };
+
+    let key = "13";
+    let timestamp = "4089402184902";
+    let location = "22.012, 105.20";
+    let vessel = "Hansdwel";
+    let holder = "tedwst";
+
+    let args = [key, timestamp, location, vessel, holder];
+
+
+    const store_path = CONSTANT.FABRIC_WALLET_PATH;
+    console.log('Store path:'+store_path);
+
+    FabricHelper.initObject(user_client)
+        .then(user_client => FabricHelper.invokeByChainCode(user_client, "recordTuna", args))
+        .then((results) => {
+        console.log('Send transaction promise and event listener promise have completed');
+        // check the results in the order the promises were added to the promise all list
+        if (results && results[0] && results[0].status === 'SUCCESS') {
+            console.log('Successfully sent transaction to the orderer.');
+            console.log(user_client.tx_id.getTransactionID());
+        } else {
+            console.error('Failed to order the transaction. Error code: ');
+        }
+
+        if(results && results[1] && results[1].event_status === 'VALID') {
+            console.log('Successfully committed the change to the ledger by the peer');
+            console.log(user_client.tx_id.getTransactionID());
+        } else {
+            console.log('Transaction failed to be committed to the ledger due to ::'+results[1].event_status);
+        }
+    }) // 결과
+        .catch((err) => {
+        console.error('Failed to invoke successfully :: ' + err);
+    });
+}
+
+main();
