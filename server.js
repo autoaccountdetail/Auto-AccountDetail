56f6edb8f5b0a5208233c1438f9b20680848371b
LeeSeungCheon <koogk7@gmail.com>
2019-09-14 23:30:02 +0900

    포트번호 체인지

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require('./route');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 const server = app.listen(port, function () {
     console.log("서버 기동 : "+ port);
